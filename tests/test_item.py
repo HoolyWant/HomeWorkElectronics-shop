9bd4199f2c4d228f11997a5d7c574ff42e6c0d1a
hoolywant <sir.saltickow@yandex.ru>
2023-06-06 16:11:11 +0300

    added some tests and fixes, unfortunately coverage only 95%

--- tests/test_item.py
+++ tests/test_item.py
@@ -7,6 +7,7 @@ import pytest
 def test_item():
     return Item('Phone', 5000, 10)
 
+
 @pytest.fixture
 def test_item2():
     return Item('Dog', '2', 0)
@@ -16,6 +17,7 @@ def test_correct_datas(test_item2):
     with pytest.raises(TypeError):
         test_item2.apply_discount()
 
+
 def test_calculate_total_price(test_item):
     assert test_item.calculate_total_price() == 50000
 
@@ -24,4 +26,21 @@ def test_apply_discount(test_item):
     test_item.pay_rate = 0.8
     test_item.apply_discount()
     assert test_item.price == 4000
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5.111111') == 5
+    assert Item.string_to_number('6764') == 6764
+    assert Item.string_to_number('6.9999') == 6
+
+
+def test_set_name():
+    with pytest.raises(Exception):
+        test_item3 = Item('BigGun', 1000000, 1)
+        test_item3.name('BigFuckingGun')
+
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
 
