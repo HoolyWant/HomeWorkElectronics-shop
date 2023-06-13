079d25ea38e81f0af5ff5a853071930a52efc4a1
hoolywant <sir.saltickow@yandex.ru>
2023-06-13 19:59:12 +0300

    added new class

--- src/phone.py
+++ src/phone.py
@@ -4,7 +4,10 @@ from src.item import Item
 class Phone(Item):
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
         super().__init__(name, price, quantity)
-        self.number_of_sim = number_of_sim
+        if number_of_sim > 0 and type(number_of_sim) == int:
+            self.number_of_sim = number_of_sim
+        raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
 
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
